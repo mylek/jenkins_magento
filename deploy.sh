db1a33be0d15f7310c02205a1e429cf1af84f6fe
mylek <mylek89@gmail.com>
2025-03-03 14:55:15 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ cd /var/www/html
 
 mkdir releases/$RELEASE
 ln -sf share/var releases/$RELEASE/var
+ln -sf share/var releases/$RELEASE/app/etc/env.php
+ln -sf share/pub/media releases/$RELEASE/pub/media
 ### komendy magento
 
 
