0b9afcafeb16d23238830eaca5a2e25008bef24e
mylek <mylek89@gmail.com>
2025-03-04 11:26:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 RELEASE=$1
 cd /var/www/html
 
+### create release dir and unzip
 mkdir releases/$RELEASE
 tar -xzf tmp/$RELEASE.tar.gz -C releases/$RELEASE --strip-components=1
 
@@ -10,17 +11,20 @@ ln -sf share/env.php releases/$RELEASE/app/etc/env.php
 ln -sf share/pub/media releases/$RELEASE/pub/media
 
 ### komendy magento
+cd releases/$RELEASE
 echo "bin/magento setup:upgrade --keep-generated"
-
+cd /var/www/html
 
 ### create core symlink
+rm current
 ln -sf releases/$RELEASE current
 
+### remove archived files
 rm -rf tmp/$RELEASE.tar.gz
 
 ### Remove dir not 3 last
 cd releases
 find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
-cd ..
+cd /var/www/html
 
 exit
