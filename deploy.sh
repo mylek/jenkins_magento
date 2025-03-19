3258259e29430b0b66493d0a901ad2ad60ee548d
mylek <mylek89@gmail.com>
2025-03-19 22:48:10 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 RELEASE=$1
-cd /var/www/html
+cd /var/www/spamgwozd.chickenkiller.com
 
 ### create release dir and unzip
 mkdir releases/$RELEASE
@@ -13,7 +13,7 @@ ln -sf share/pub/media releases/$RELEASE/pub/media
 ### komendy magento
 cd releases/$RELEASE
 echo "bin/magento setup:upgrade --keep-generated"
-cd /var/www/html
+cd /var/www/spamgwozd.chickenkiller.com
 
 ### create core symlink
 rm current
@@ -28,6 +28,6 @@ rm -rf tmp/$RELEASE.tar.gz
 ### Deletes old releases folders leaving the last 3
 cd releases
 find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
-cd /var/www/html
+cd /var/www/spamgwozd.chickenkiller.com
 
 exit
