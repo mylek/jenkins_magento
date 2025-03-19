e0169c0a41b65bb7aaa71c64a91ba9d448acbc50
mylek <mylek89@gmail.com>
2025-03-19 23:44:48 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,34 @@
 RELEASE=$1
-cd /var/www/spamgwozd.chickenkiller.com
+SERVER_DIR=$2
+cd $SERVER_DIR
 
 ### create release dir and unzip
 mkdir releases/$RELEASE
 tar -xzf tmp/$RELEASE.tar.gz -C releases/$RELEASE --strip-components=1
 
 ### create assets symlinks
-ln -sf share/var releases/$RELEASE/var
-ln -sf share/env.php releases/$RELEASE/app/etc/env.php
-ln -sf share/pub/media releases/$RELEASE/pub/media
+ln -sf $SERVER_DIR/share/var $SERVER_DIR/releases/$RELEASE/var
+ln -sf $SERVER_DIR/share/env.php $SERVER_DIR/releases/$RELEASE/app/etc/env.php
+ln -sf $SERVER_DIR/share/pub/media $SERVER_DIR/releases/$RELEASE/pub/media
 
 ### komendy magento
-cd releases/$RELEASE
+cd $SERVER_DIR/releases/$RELEASE
 echo "bin/magento setup:upgrade --keep-generated"
-cd /var/www/spamgwozd.chickenkiller.com
+cd $SERVER_DIR
 
 ### create core symlink
-sudo rm -fr current
-ln -sf releases/$RELEASE current
+sudo rm -fr $SERVER_DIR/current
+ln -sf $SERVER_DIR/releases/$RELEASE current
 
 ### restart services
 echo "sudo /etc/init.d/php8.1-fpm restart"
 
 ### remove archived files
-rm -rf tmp/$RELEASE.tar.gz
+rm -rf $SERVER_DIR/tmp/$RELEASE.tar.gz
 
 ### Deletes old releases folders leaving the last 3
-cd releases
+cd $SERVER_DIR/releases
 find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
-cd /var/www/spamgwozd.chickenkiller.com
+cd $SERVER_DIR
 
 exit
