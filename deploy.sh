31e5c2a0a75b2859ed7214d48147d99a7b645294
mylek <mylek89@gmail.com>
2025-03-20 08:04:35 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ mkdir $SERVER_DIR/releases/$RELEASE
 tar -xzf $SERVER_DIR/tmp/$RELEASE.tar.gz -C releases/$RELEASE --strip-components=1
 
 ### create assets symlinks
-ln -sf $SERVER_DIR/share/var/ $SERVER_DIR/releases/$RELEASE/var/
+ln -sf $SERVER_DIR/share/var/ $SERVER_DIR/releases/$RELEASE/
 ln -sf $SERVER_DIR/share/env.php $SERVER_DIR/releases/$RELEASE/app/etc/env.php
-ln -sf $SERVER_DIR/share/pub/media $SERVER_DIR/releases/$RELEASE/pub/media
+ln -sf $SERVER_DIR/share/pub/media $SERVER_DIR/releases/$RELEASE/pub/
 
 ### komendy magento
 cd $SERVER_DIR/releases/$RELEASE
@@ -18,7 +18,7 @@ cd $SERVER_DIR
 
 ### create core symlink
 sudo rm -fr $SERVER_DIR/current
-ln -sf $SERVER_DIR/releases/$RELEASE $SERVER_DIR/current
+ln -sf $SERVER_DIR/releases/$RELEASE $SERVER_DIR
 
 ### restart services
 echo "sudo /etc/init.d/php8.1-fpm restart"
