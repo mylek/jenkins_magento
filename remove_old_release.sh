1003082ad3f822d7729945ea62d63e5e4c87e72f
mylek <mylek89@gmail.com>
2025-03-20 13:40:42 +0100

    Update remove_old_release.sh

--- remove_old_release.sh
+++ remove_old_release.sh
@@ -2,6 +2,6 @@ SERVER_DIR=$1
 
 ### Deletes old releases folders leaving the last 3
 cd $SERVER_DIR/releases
-find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
+sudo find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
 
 exit
