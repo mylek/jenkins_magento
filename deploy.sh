77002497d9941ca6701a415bef2bae2a7b760dd4
mylek <mylek89@gmail.com>
2025-03-20 08:25:05 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ mkdir $SERVER_DIR/releases/$RELEASE
 tar -xzf $SERVER_DIR/tmp/$RELEASE.tar.gz -C releases/$RELEASE --strip-components=1
 
 ### create assets symlinks
-ln -sf $SERVER_DIR/share/var/ $SERVER_DIR/releases/$RELEASE/
-ln -sf $SERVER_DIR/share/env.php $SERVER_DIR/releases/$RELEASE/app/etc/env.php
-ln -sf $SERVER_DIR/share/pub/media $SERVER_DIR/releases/$RELEASE/pub/
+#ln -sf $SERVER_DIR/share/var/ $SERVER_DIR/releases/$RELEASE/
+#ln -sf $SERVER_DIR/share/env.php $SERVER_DIR/releases/$RELEASE/app/etc/env.php
+#ln -sf $SERVER_DIR/share/pub/media $SERVER_DIR/releases/$RELEASE/pub/
 
 ### komendy magento
 cd $SERVER_DIR/releases/$RELEASE
