55b708975132ec2e00904e06184358dd18a5a462
mylek <mylek89@gmail.com>
2025-03-20 09:51:03 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,11 +25,11 @@ tar -xzf $SERVER_DIR/tmp/$RELEASE.tar.gz -C releases/$RELEASE --strip-components
 #echo "sudo /etc/init.d/php8.1-fpm restart"
 
 ### remove archived files
-#rm -rf $SERVER_DIR/tmp/$RELEASE.tar.gz
+rm -rf $SERVER_DIR/tmp/$RELEASE.tar.gz
 
 ### Deletes old releases folders leaving the last 3
-#cd $SERVER_DIR/releases
-#find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
-#cd $SERVER_DIR
+cd $SERVER_DIR/releases
+find . -maxdepth 1 -mindepth 1 -type d -printf "%T+ %f\0" | sort -z | head -z -n -3 | cut -z -d' ' -f 2- | xargs -0 rm -rf
+cd $SERVER_DIR
 
 exit
